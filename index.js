cb61827e74177ffb6967cea02fc4e497f629c5c8
brandon <williamsbrand879@yahoo.com>
2022-04-09 17:24:03 +0530

    make port change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { urlencoded } = require("body-parser");
 
 
 const app=express();
- const port=process.env.port||3000;
+ const port=process.env.PORT || 3000;
  app.use(bodyParser.urlencoded({extended:false}));
  app.use(express.static("static"));
  app.set("view engine","ejs")
